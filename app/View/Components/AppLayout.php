34f101a714e1dbcbfa95afe9efa8e785a8da8f56
mahesmaheswara <111202214594@mhs.dinus.ac.id>
2026-01-21 18:07:17 +0700

    refactor: improve app and guest layout components

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -1,17 +1,61 @@
 <?php
 
+/**
+ * ==========================================================
+ * JUDUL  : AppLayout Component (Layout Utama Aplikasi)
+ * LOKASI : app/View/Components/AppLayout.php
+ * ==========================================================
+ *
+ * FUNGSI:
+ * View Component ini digunakan sebagai layout utama
+ * untuk halaman yang diakses oleh user yang sudah login.
+ *
+ * TUJUAN:
+ * - Menyatukan struktur halaman (navbar, footer, dll)
+ * - Menghindari duplikasi kode layout di setiap view
+ * - Menjaga konsistensi tampilan aplikasi
+ *
+ * DIGUNAKAN OLEH:
+ * - <x-layouts.app>
+ * - Halaman home
+ * - Halaman order
+ * - Halaman profile
+ *
+ * KAMUS UMUM:
+ * - View Component : Komponen reusable untuk tampilan
+ * - Layout         : Kerangka utama halaman
+ */
+
 namespace App\View\Components;
 
+// Base class untuk View Component
 use Illuminate\View\Component;
+
+// Tipe return View
 use Illuminate\View\View;
 
 class AppLayout extends Component
 {
     /**
-     * Get the view / contents that represents the component.
+     * ==================================================
+     * [METHOD] render()
+     * ==================================================
+     * FUNGSI:
+     * - Menentukan file blade yang digunakan
+     *   sebagai layout
+     *
+     * RETURN:
+     * - components/layouts/app.blade.php
+     *
+     * CARA PAKAI DI VIEW:
+     * <x-layouts.app>
+     *    konten halaman
+     * </x-layouts.app>
      */
     public function render(): View
     {
-        return view('components.layouts.app');
+        return view(
+            'components.layouts.app'
+        );
     }
 }
--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -1,17 +1,60 @@
 <?php
 
+/**
+ * ==========================================================
+ * JUDUL  : GuestLayout Component (Layout Tamu)
+ * LOKASI : app/View/Components/GuestLayout.php
+ * ==========================================================
+ *
+ * FUNGSI:
+ * View Component ini digunakan sebagai layout
+ * untuk halaman yang diakses oleh user
+ * yang BELUM login.
+ *
+ * TUJUAN:
+ * - Menyediakan layout sederhana untuk halaman auth
+ * - Memisahkan tampilan guest dan user login
+ * - Menjaga keamanan & UX aplikasi
+ *
+ * DIGUNAKAN OLEH:
+ * - Halaman login
+ * - Halaman register
+ * - Halaman reset password
+ *
+ * KAMUS UMUM:
+ * - Guest : Pengunjung yang belum login
+ * - Layout: Kerangka tampilan halaman
+ */
+
 namespace App\View\Components;
 
+// Base class untuk View Component
 use Illuminate\View\Component;
+
+// Tipe return View
 use Illuminate\View\View;
 
 class GuestLayout extends Component
 {
     /**
-     * Get the view / contents that represents the component.
+     * ==================================================
+     * [METHOD] render()
+     * ==================================================
+     * FUNGSI:
+     * - Menentukan file blade layout untuk guest
+     *
+     * RETURN:
+     * - components/layouts/guest.blade.php
+     *
+     * CARA PAKAI DI VIEW:
+     * <x-layouts.guest>
+     *    konten halaman auth
+     * </x-layouts.guest>
      */
     public function render(): View
     {
-        return view('components.layouts.guest');
+        return view(
+            'components.layouts.guest'
+        );
     }
 }
